80f461d0e193c2c64e9706785d20d818cfba0048
Hayden <63182335+haydenhuyp@users.noreply.github.com>
2022-02-05 10:18:16 -0500

    SignIn Page added (.cshtml and .css)

--- RecipeSuggestion/Controllers/HomeController.cs
+++ RecipeSuggestion/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace RecipeSuggestion.Controllers
         {
             return View();
         }
+        public IActionResult SignIn()
+		{
+            return View();
+		}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
