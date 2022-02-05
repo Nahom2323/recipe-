be99d48e644c722920a359c60d1c9e7ba931dcab
ljh0354 <juho1114@naver.com>
2022-02-05 09:54:36 -0500

    Jooho

---
+++ RecipeSuggestion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeSuggestion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
